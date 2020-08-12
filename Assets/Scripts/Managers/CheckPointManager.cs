e00c8199549f96a1831fabb95f0cd5cff1bd2991
Furkanybk <37793163+Furkanybk@users.noreply.github.com>
2020-08-12 16:47:30 +0300

    collider & char added

---
+++ Assets/Scripts/Managers/CheckPointManager.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+
+public enum CheckPointType
+{
+    Checkpoint,
+    Startpoint,
+    Endpoint
+};
+
+public class CheckPointManager : MonoBehaviour
+{
+    [SerializeField]
+    public CheckPointType type = CheckPointType.Checkpoint; 
+    [SerializeField]
+    private bool IsCheckpointPassed = false; 
+    [SerializeField]
+    private Material material;
+
+    private Vector3 CheckpointPos;
+
+    //[SerializeField]
+    //public List<SpawnOrc> spawnList = new List<SpawnOrc>();
+    //[SerializeField]
+    //public List<SpawnOrc> deSpawnList = new List<SpawnOrc>();
+
+    //[SerializeField]
+    //private List<GameObject> Lambs = new List<GameObject>();
+
+
+    private void Awake()
+    {
+        //for (int i = 0; i < 4; i++)
+        //{ 
+        //    Lambs.Add(transform.GetChild(1).GetChild(i).gameObject);
+        //} 
+    }
+    private void Start()
+    {
+        CheckpointPos = transform.position;
+        IsCheckpointPassed = false;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (IsCheckpointPassed) return;
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            IsCheckpointPassed = true;
+
+            //foreach(SpawnOrc spawn in spawnList)
+            //{
+            //    spawn.Spawn();
+            //}
+            //foreach(SpawnOrc deSpawn in deSpawnList)
+            //{
+            //    deSpawn.deSpawn();
+            //}
+
+            //foreach (var lamb in Lambs)
+            //{
+            //    lamb.GetComponentInChildren<Renderer>().materials[1].color = material.color;
+            //}
+            switch (type)
+            {
+                case CheckPointType.Checkpoint: 
+                    GameManager.current.ReachedCheckPoint++;
+                    GameManager.current.lastCheckpoint = this;
+                    GameManager.current.Text_ReachedCheckPoint.text = "CheckPoint Saved : " + GameManager.current.ReachedCheckPoint; 
+                    if (!IsCheckpointPassed)
+                    {
+                        IsCheckpointPassed = true;
+                        GameManager.current.ReachedCheckPoint++;
+                        GameManager.current.PlayerLastCheckPoint = CheckpointPos;
+                    }
+
+                    GameManager.current.Text_ReachedCheckPoint.text = "CheckPoint Saved : " + GameManager.current.ReachedCheckPoint; 
+                    switch (GameManager.current.ReachedCheckPoint)
+                    {
+                        case 1:
+                            GameManager.current.CheckPointInfo = "NICE";
+                            break;
+                        case 2:
+                            GameManager.current.CheckPointInfo = "GOOD";
+                            break;
+                        case 3:
+                            GameManager.current.CheckPointInfo = "YOU GOT THIS";
+                            break;
+                        case 4:
+                            GameManager.current.CheckPointInfo = "WOW";
+                            break;
+                        case 5:
+                            GameManager.current.CheckPointInfo = "PERFECT";
+                            break;
+                        case 6:
+                            GameManager.current.CheckPointInfo = "GOOD SLIDE";
+                            break;
+                        case 7:
+                            GameManager.current.CheckPointInfo = "AGILE";
+                            break;
+                        case 8:
+                            GameManager.current.CheckPointInfo = "WHAT A MOVEMENT";
+                            break;
+                        case 9:
+                            GameManager.current.CheckPointInfo = "PERFECT SLIDE";
+                            break;
+                        case 10:
+                            GameManager.current.CheckPointInfo = "ALMOST DONE";
+                            break;
+                        case 11:
+                            GameManager.current.CheckPointInfo = "UNSTOPPABLE";
+                            break; 
+                    }  
+                    break;
+                case CheckPointType.Startpoint:
+                    Debug.Log("START");
+                    break;
+                case CheckPointType.Endpoint:
+                    //GameMenu.current.Complete();
+                    GameManager.current.IsGameFinish = true; 
+                    GameManager.current.Timer.Stop();
+                    gameObject.transform.GetChild(0).GetChild(1).gameObject.SetActive(true);
+                    break;
+            }
+            GameManager.current.Text_CheckPoint.enabled = true;
+            GameManager.current.Text_CheckPoint.text = GameManager.current.CheckPointInfo;
+            StartCoroutine(closeText(1.5f));
+        }
+    }
+
+    private IEnumerator closeText(float time)
+    {
+        yield return new WaitForSeconds(time);
+        GameManager.current.Text_CheckPoint.enabled = false;
+    }
+}
---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,125 @@
+﻿using Cinemachine;
+using System;
+using System.Collections;
+using System.Diagnostics;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+using Debug = UnityEngine.Debug;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager current = null; 
+
+    public CheckPointManager lastCheckpoint;
+    public float CameraLastPathWay; 
+     
+    public Vector3 PlayerLastCheckPoint; 
+    public static int TotalChance = 3;
+    public int RemainingChance = TotalChance; 
+    public int ReachedCheckPoint = 0;
+    [Space] 
+    public CinemachineVirtualCamera camera; 
+    [Space]
+    public GameObject Ninja;
+    [Space]
+    public TextMeshProUGUI Text_CheckPoint;
+    public GameObject UX_RespawnNumber;
+    public TextMeshProUGUI Text_ReachedCheckPoint;
+    public TextMeshProUGUI Text_Timer;
+
+    [HideInInspector]
+    public bool IsRespawned = false;
+
+    [HideInInspector]
+    public string CheckPointInfo;
+
+    [HideInInspector]
+    public Stopwatch Timer = new Stopwatch();
+
+    [HideInInspector]
+    public bool IsPlayerStart = false; 
+
+    [HideInInspector]
+    public bool IsGameFinish = false;  
+
+
+    private void Awake()
+    {  
+        if (current != null)
+        { 
+            Debug.Log("Already a GameManager Working.");
+        }
+        else
+        {
+            current = this;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (current == this)
+            current = null;
+    }
+
+    private void Start()
+    {  
+        SpawnPlayer(Vector3.zero); 
+    }
+
+    private void Update()
+    {
+        if(IsPlayerStart && !Timer.IsRunning && !IsGameFinish)
+        {
+            Timer.Start(); 
+        }
+        if(Text_Timer && Timer.IsRunning)
+        {
+            TimeSpan ts = Timer.Elapsed; 
+            Text_Timer.text = string.Format("{0:00}:{1:00}:{2:00}",ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+        }
+    }
+
+    public void Respawn()
+    {
+        IsRespawned = true;
+        Destroy(GameObject.FindGameObjectWithTag("Player"));
+        if (lastCheckpoint)
+            SpawnPlayer(lastCheckpoint.transform.position);
+        else
+            SpawnPlayer(Vector3.zero);
+
+        switch (RemainingChance)
+        { 
+            case 1:
+                CheckPointInfo = "LAST CHANCE";
+                break;
+            case 2:
+                CheckPointInfo = "WATCH OUT";
+                break; 
+            default:
+                break;
+        } 
+        Text_CheckPoint.enabled = true;
+        Text_CheckPoint.text = CheckPointInfo;
+        StartCoroutine(closeText(1.5f));
+    }
+
+    private void SpawnPlayer(Vector3 pos)
+    {
+        pos.y += 0.25f;
+        GameObject ninja = Instantiate(Ninja, pos, Quaternion.identity);
+        ninja.transform.position = pos;
+        SetCamera(ninja);
+    }
+    private void SetCamera(GameObject ninja)
+    {
+        camera.Follow = ninja.transform;
+        camera.LookAt = ninja.transform; 
+    } 
+    private IEnumerator closeText(float time)
+    {
+        yield return new WaitForSeconds(time);
+        GameManager.current.Text_CheckPoint.enabled = false;
+    }
+} 

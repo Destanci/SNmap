2f2e21769506511950dd682635c4d63a05e4bd05
Furkanybk <37793163+Furkanybk@users.noreply.github.com>
2020-08-26 11:22:31 +0300

    x

---
+++ Assets/Scripts/Managers/CameraFollow.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{ 
+    public Transform Target; 
+    private void LateUpdate()
+    {  
+        transform.LookAt(Target);
+    }
+}
